c761e4176fa2c018787f43e0bdc9c3b2303d94b7
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-08-02 10:38:12 +0900

    add SearchMbti.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import Heading from './components/Heading'
+import './App.css';
+import Heading from './components/Heading';
+import SearchMbti from './components/SearchMbti';
 
 function App() {
   return (
     <div className="mainContainer">
       <Heading />
+      <SearchMbti />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/SearchMbti.jsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from 'react';
+
+const validMbti = [
+    'ISTJ', 'ISFJ', 'INFJ', 'INTJ',
+    'ISTP', 'ISFP', 'INFP', 'INTP',
+    'ESTP', 'ESFP', 'ENFP', 'ENTP',
+    'ESTJ', 'ESFJ', 'ENFJ', 'ENTJ'
+];
+
+const SearchMbti = () => {
+    const inputRef = useRef();
+    const [feedback, setFeedback] = useState('');
+    
+    const chkValidMbti = () => {
+        if (inputRef.current !== null) {
+            const input = inputRef.current.value.trim().toUpperCase();
+            if (validMbti.includes(input)) {
+                setFeedback(`${input}로 변할게요!`)
+            } else {
+                setFeedback("올바른 MBTI를 입력해주세요!")
+            }
+        } else {
+            setFeedback('MBTI를 입력해주세요!')
+        }
+    }
+
+    return (
+        <div className=''>
+            <input type="text" placeholder="MBTI를 검색하세요!" id="searchMbti" ref={inputRef} />
+            <button onClick={chkValidMbti}>선택</button>
+            {feedback && <p>{feedback}</p>}
+        </div>
+    )
+
+};
+
+export default SearchMbti;
