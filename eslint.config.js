292f2a59d364d415ad1ec7db4d0632049162a626
Jae <jaeyeonjung3@gmail.com>
2024-09-22 16:51:59 +0900

    eslint 변경

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: parser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
